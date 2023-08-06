f3cb8a57268a5b024e55496dedf8ae0f25858214
Utkarsh Padia <utkarshpadia@gmail.com>
2023-08-06 11:24:37 +0530

    chore: video upload

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 from moviepy.editor import *
 import openai
+from youtube_transcript_api import YouTubeTranscriptApi
 import speech_recognition as sr
 app = Flask(__name__)
 CORS(app)
@@ -16,13 +17,39 @@ def read_movie(movie):
 @app.route('/upload_link',methods = ['POST'])
 def upload_link():
     link = request.get_json()
-    print ("link ",link)
+    video_id = link['link'].split('/')[3]
+    print (video_id)
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id= video_id)
+    except:
+        try:
+            video_id = link['link'].split('/')[4].split('?')[0]
+            transcript_list = YouTubeTranscriptApi.list_transcripts(video_id=video_id)
+        except:
+            return {'err':'Transcript not found'}
+    try:
+        transcript = transcript_list.find_manually_created_transcript(['en'])
+    except:
+        try:
+            transcript = transcript_list.find_generated_transcript(['en'])
+        except:
+            return {'err':'English Transcript not found'}
+
+    complete_transcript = transcript.fetch()
+    transcript_string = ''
+    for i in complete_transcript:
+        transcript_string += i['text'] + ' '
+    print (transcript_string)
     return '200'
 
 @app.route('/upload_video',methods = ['POST'])
 def upload_video():
-    file1 = request.files[0]
-    video_file = VideoFileClip(file1)
+    # print ("hello")
+    # file1 = request.files
+    # print ("before video clip")
+    # video_file = VideoFileClip('movie1.mov')
+    # print ("after video clip"
+    print ("before 200")
     return '200'
 
 
--- video-ui/src/app/Components/upload/upload.component.ts
+++ video-ui/src/app/Components/upload/upload.component.ts
@@ -7,18 +7,19 @@ import { VideoService } from 'src/app/services/video.service';
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
-  
-  constructor(private service:VideoService) { }
+
+  constructor(private service: VideoService) { }
   tubeUrl: any = ''
-  file01:File | undefined;
-  submitLink(event:Event){
+  // file01: File | undefined;
+  file01: any 
+  submitLink(event: Event) {
     console.log("inside submit link")
-    if (this.tubeUrl==''){
+    if (this.tubeUrl == '') {
       alert("Please enter a video link")
       return
     }
-    else{
-      this.service.uploadLink({"link":this.tubeUrl}).subscribe(
+    else {
+      this.service.uploadLink({ "link": this.tubeUrl }).subscribe(
         data => {
           console.log(data)
         },
@@ -28,31 +29,33 @@ export class UploadComponent implements OnInit {
       )
 
     }
-    
-  
+
+
     console.log(this.tubeUrl)
   }
 
-  submitVideo(event:Event){
-    console.log("upload video ",this.file01)
-    if (this.file01?.type.includes('video')){
+  submitVideo(event: Event) {
+    console.log("upload video ", this.file01)
+    // if (this.file01?.type.includes('video')) {
+      if (true){
       console.log('video')
-      this.service.uploadVideo(this.file01).subscribe( res=> {console.log(res)}
-      ,err => {console.log("error  ",err)})
+      this.service.uploadVideo(this.file01).subscribe(res => { console.log('respnse   ', res) }
+        , err => { console.log("error  ", err) })
     }
-    else{
+    else {
       alert("Please upload a video")
     }
   }
-  fileUpload(event:any){
-    console.log(" file 1 ",event?.target?.files[0].type)
+  fileUpload(event: any) {
+    console.log(" file 1 ", event?.target?.files[0].type)
     this.file01 = event?.target?.files[0]
-    if (event?.target?.files[0].type.includes('video')){
-      console.log('video')
-    }
-    else{
-      alert("Please upload a video")
-    }
+    // if (event?.target?.files[0].type.includes('video')) {
+    //   if (1){
+    //   console.log('video')
+    // }
+    // else {
+    //   alert("Please upload a video")
+    // }
   }
   ngOnInit(): void {
   }
