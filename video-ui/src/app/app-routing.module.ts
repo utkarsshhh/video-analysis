d27b7e9c7cdb719c8650e200e193d05213e279c2
Utkarsh Padia <utkarshpadia@gmail.com>
2023-08-18 23:34:46 +0530

    chore: share transcript

--- video-ui/src/app/app-routing.module.ts
+++ video-ui/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TranscriptComponent } from './Components/transcript/transcript.component';
 import { UploadComponent } from './Components/upload/upload.component';
 
-const routes: Routes = [{path:'upload',component:UploadComponent},{path:'**',redirectTo:'upload'}];
+const routes: Routes = [{path:'upload',component:UploadComponent},{path:'transcript',component:TranscriptComponent},{path:'**',redirectTo:'upload'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
